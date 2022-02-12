b787ee3cb11666c5d30a7d627e838031b9aca497
KH Rasedul Islam <87899184+khrasedul-dev@users.noreply.github.com>
2022-02-12 17:12:25 +0600

    Update app.js

--- app.js
+++ app.js
@@ -36,7 +36,22 @@ bot.hears('💰 Buy',ctx=>{
     ctx.reply("This is Buy")
 })
 
+bot.command('buy',ctx=>{
+    ctx.reply("This is Buy")
+})
+
 bot.hears('🌎 Website',ctx=>{
+    ctx.telegram.sendMessage(ctx.chat.id , `<b>What Do We Do?</b>\nWe want to blur the line between the cryptocurrency merchants and ordinary consumers. We will popularize the cryptocurrency by giving free admission, and by helping people understand and confide what we believe to be the future of money.` , {
+        reply_markup:{
+            inline_keyboard:[
+                [{text: "Visit our website", url: "https://gravitymetaverse.io"}]
+            ]
+        },
+        parse_mode: "HTML"
+    }).catch("Something is wrong")
+})
+
+bot.command('website',ctx=>{
     ctx.telegram.sendMessage(ctx.chat.id , `<b>What Do We Do?</b>\nWe want to blur the line between the cryptocurrency merchants and ordinary consumers. We will popularize the cryptocurrency by giving free admission, and by helping people understand and confide what we believe to be the future of money.` , {
         reply_markup:{
             inline_keyboard:[
@@ -51,7 +66,16 @@ bot.hears('🚀 RoadMap',ctx=>{
     ctx.reply("This is Roadmap")
 })
 
+bot.command('roadmap',ctx=>{
+    ctx.reply("This is Roadmap")
+})
+
+
 bot.hears('💵 Slippage',ctx=>{
+    ctx.reply("This is slippage")
+})
+
+bot.command('slippage',ctx=>{
     ctx.reply("This is slippage")
 })
 
@@ -59,7 +83,15 @@ bot.hears('📝 WhitePaper',ctx=>{
     ctx.reply("This is WhitePaper")
 })
 
+bot.command('whitepaper',ctx=>{
+    ctx.reply("This is WhitePaper")
+})
+
 bot.hears('📜 Contract',ctx=>{
+    ctx.replyWithHTML(`<b>Contract Us:</b> \nWebsite: https://gravitymetaverse.io/\nInstagram: gravitymetaverse\nFacebook: Gravity Metaverse`).catch("Somthing is wrong")
+})
+
+bot.command('contract',ctx=>{
     ctx.replyWithHTML(`<b>Contract Us:</b> \nWebsite: https://gravitymetaverse.io/\nInstagram: gravitymetaverse\nFacebook: Gravity Metaverse`).catch("Somthing is wrong")
 })
 
