7591dd1946346bc4e0244fed881a2491f36c4e43
KH Rasedul Islam <87899184+khrasedul-dev@users.noreply.github.com>
2022-02-12 16:39:13 +0600

    Update app.js

--- app.js
+++ app.js
@@ -22,12 +22,13 @@ bot.hears('💰 Buy',ctx=>{
 })
 
 bot.hears('🌎 Website',ctx=>{
-    ctx.telegram.sendMessage(ctx.chat.id , `What Do We Do?\nWe want to blur the line between the cryptocurrency merchants and ordinary consumers. We will popularize the cryptocurrency by giving free admission, and by helping people understand and confide what we believe to be the future of money.` , {
+    ctx.telegram.sendMessage(ctx.chat.id , `<b>What Do We Do?</b>\nWe want to blur the line between the cryptocurrency merchants and ordinary consumers. We will popularize the cryptocurrency by giving free admission, and by helping people understand and confide what we believe to be the future of money.` , {
         reply_markup:{
             inline_keyboard:[
                 [{text: "Visit our website", url: "https://gravitymetaverse.io"}]
             ]
-        }
+        },
+        parse_mode: HTML
     })
 })
 
